963010ee1ddac19755154700f044a18625a9c02e
Troy Sargent <troyjsargent@gmail.com>
2020-08-02 21:49:45 -0500

    fixed clear button bug, added current weather/uv index card api response

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function renderBtns(){
 
 $("#clearBtn").on("click", function () {
     localStorage.clear();
-    $("#cityBtns").empty();
+    renderBtns();
 });
 
 $("#citySearch").on("submit", function (event) {
@@ -27,28 +27,58 @@ $("#citySearch").on("submit", function (event) {
     localStorage.setItem("cityList", JSON.stringify(cityList));
 
     renderBtns();
-    getWeather(cityInput);
+
+    $("#currentWeather").empty();
+    getCurrentWeather(cityInput);
+    getForecast(cityInput);
 });
 
+let apiKey = "3a2865bfdde9a3d0404b80605b03f65a";
 
-function getWeather(cityInput){
-    let apiKey = "3a2865bfdde9a3d0404b80605b03f65a";
+function getCurrentWeather(cityInput){
     let queryUrlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`;
-    let queryUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}`;
     
     $.ajax({
         url: queryUrlWeather,
         method: "GET",
     }).then(function (response) {
         console.log(response);
+
+        $(`<h2>${cityInput}</h2>
+        <p class="card-text">Temperature: ${response.main.temp}</p>
+        <p class="card-text">Humidity: ${response.main.humidity}</p>
+        <p class="card-text">Wind Speed: ${response.wind.speed}</p>`).appendTo(("#currentWeather"));
+        
+        let lat = parseInt(response.coord.lat);
+        let lon = parseInt(response.coord.lon);
+        getUVIndex(lat, lon);
     });
+};
+
+function getUVIndex(lat, lon) {
+    let queryUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+        $(`<p class="card-text">UV Index: ${response.value}</p>`).appendTo(("#currentWeather"));
+    });
+
+};
+
+function getForecast(cityInput) {
+    let queryUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}`;
     $.ajax({
         url: queryUrlForecast,
         method: "GET",
     }).then(function (response) {
         console.log(response);
+        for (var i=0; i < 5, i++){
+        response.list[i].dt_txt
+        };
     });
-    
 };
 
 renderBtns();   
